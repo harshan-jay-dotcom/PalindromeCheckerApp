2dbc87eb26ce1f699263b1f072a9c256dd95c792
harshan-jay-dotcom <hj3090@srmist.edu.in>
2026-02-23 16:39:22 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("======================================");
+        System.out.println("   Welcome to Palindrome Checker App   ");
+        System.out.println("======================================");
+        System.out.println("Application Name   : Palindrome Checker App");
+        System.out.println("Application Version: 1.0.0");
+        System.out.println("--------------------------------------");
+        System.out.println("Status: Application started successfully.");
+    }
+}
